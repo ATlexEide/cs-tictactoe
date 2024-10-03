7fc5f7bf5d0fbc8e8d4fa52734ea3bf57ab059b2
Alexander Eide <atlexeide@gmail.com>
2024-10-03 17:38:17 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace cs_tictactoe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Init empty gameboard
+        string[] gameBoard = ["","","",
+                              "","","",
+                              "","",""];
+        // Get marker input from player
+        static string getPlayerMarker(){
+        string? playerMarker = Console.ReadLine().ToLower();
+        if(playerMarker == "x" || playerMarker == "o"){
+            return playerMarker;
+        }else{
+            Console.WriteLine($"{playerMarker} is not a valid input. Expected 'x' or 'o'");
+            return getPlayerMarker();
+        }
+                };
+        // Choose markers
+        Console.WriteLine("Choose a marker: ('x' or 'o')");
+        string playerMarker = getPlayerMarker();
+        string computerMarker;
+        if(playerMarker == "x"){
+            computerMarker = "o";
+        }else{
+            computerMarker = "x";
+        };
+        Console.WriteLine($"You chose {playerMarker}, computer plays {computerMarker}");
+                            
+    }
+};
